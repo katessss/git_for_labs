778c05fc9a702ead3716fd6f2aa57eae2acbaf29
katessss <kates-10@mail.ru>
2025-12-14 20:04:05 +0700

    add test

--- lab4/test.sh
+++ lab4/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 API_URL="http://localhost:8000/test_lab4"
+
 echo "ТЕСТ 10 запросов"
 
-for i in {1..10}; do 
-    task = $i
-    id = $((i/2))
-    echo "task $task | id $id"
-    curl -s -X POST "$API_URL" \
-        -H "Content-Type: application/json" 
-        -d "{\"id\": \"$id\", \"item\": \"$task\"}" 
+for i in {1..10}; do
+  task=$i
+  id=$((i/2))
+  echo "task $task | id $id"
+  
+  curl -s -X POST "$API_URL" -H "Content-Type: application/json" -d "{\"order_id\": \"$id\", \"item\": \"$task\"}"
 done
